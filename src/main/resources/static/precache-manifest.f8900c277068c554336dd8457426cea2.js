96c253f9c7fe90ef118a9ec7cc00c2bad7c481ae
Selma Kahvedzic <selma.kahvedzic@ping.ba>
2022-03-15 15:19:22 +0100

    Uvezano sa frontendom

---
+++ src/main/resources/static/precache-manifest.f8900c277068c554336dd8457426cea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640eda995e583a004cc7c438e9a5709b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30047fd4ba3ca64e55c0",
+    "url": "/static/css/2.5711c12b.chunk.css"
+  },
+  {
+    "revision": "5aa11307d5b3661e8496",
+    "url": "/static/css/main.b5fec8d3.chunk.css"
+  },
+  {
+    "revision": "30047fd4ba3ca64e55c0",
+    "url": "/static/js/2.66cd1fc0.chunk.js"
+  },
+  {
+    "revision": "c346956c1449420dcbe7f2a51cdfc9c4",
+    "url": "/static/js/2.66cd1fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa11307d5b3661e8496",
+    "url": "/static/js/main.1bcfa557.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
