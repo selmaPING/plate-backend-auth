002422f51ae41d5fcfa4353aaba2f7b22dac8a16
Selma Kahvedzic <selma.kahvedzic@ping.ba>
2022-04-04 15:55:35 +0200

    Unauthorized error removed

--- src/main/java/main/employees/controller/RadniciController.java
+++ src/main/java/main/employees/controller/RadniciController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8082")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class RadniciController {
